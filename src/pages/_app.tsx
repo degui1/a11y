e299ff31d261308d11b7460442b029b7027378d8
degui1 <gui.denez56@gmail.com>
2024-11-08 08:06:05 -0300

    Solving accessibility issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import "@/styles/globals.css";
+import React from "react";
 import type { AppProps } from "next/app";
+
+import "@/styles/globals.css";
+import { reportAccessibility } from "@/utils/axeAccessibilityReporter";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+reportAccessibility(React)
